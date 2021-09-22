210343249df249ca6df2269eb85d7c477e1662bf
Joshua S <75865754+Jshoea@users.noreply.github.com>
2021-09-22 13:44:47 -0700

    Add files via upload

---
+++ 10.3_arrow_hw.js
@@ -0,0 +1,3 @@
+const double = arr => arr.map(val => val * 2); //we dont need parens or the return cuz its only one variable and one action
+
+const squareAndFindEvens = numbers => numbers.map(num => num**2).filter(square => square % 2 === 0);
